6771e88af3bed75cd787876a9a6e9b68aac5ea80
bakafox <v.chekhovskii@g.nsu.ru>
2024-03-06 03:52:51 +0700

    Перенос демо-файлов в верную директорию (упс\!)

--- tasklist/demo.js
+++ tasklist/demo.js
@@ -1,4 +1,18 @@
-function toggleTaskmanView() {
-    document.getElementById("taskman-wrapper").classList.toggle("closed");
-    document.getElementById("taskman").innerHTML = '';
+function toggleDebugView() {
+    document.querySelector("#debug").style.display = 'none';
+}
+
+function toggleBoardmanView() {
+    document.querySelectorAll(".boardman-spacer").forEach(e => e.classList.toggle('closed'));
+    document.querySelector("#boardman-wrapper").classList.toggle('closed');
+}
+
+function openTaskmanView() {
+    document.querySelectorAll(".taskman-spacer").forEach(e => e.classList.remove('closed'));
+    document.querySelector("#taskman-wrapper").classList.remove('closed');
+}
+
+function closeTaskmanView() {
+    document.querySelectorAll(".taskman-spacer").forEach(e => e.classList.add('closed'));
+    document.querySelector("#taskman-wrapper").classList.add('closed');
 }
